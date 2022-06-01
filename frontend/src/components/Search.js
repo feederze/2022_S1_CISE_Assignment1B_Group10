5f3da7bfa5ea3d611ed75945ce8aee370f638f76
feederze <46537990+feederze@users.noreply.github.com>
2022-06-02 01:04:37 +1200

    search bug fix

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import evidence from './evidenceTable';
-import { response } from 'express';
+import React, { Component } from "react";
+import axios from "axios";
+// import evidence from "./evidenceTable";
+// import { response } from "express";
 
-class Search extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-           article: []
-        };
-    }
-    Search(Searchinput){
-        let postdate = {
-            "title": Searchinput
-        }
-        axios
-        .post('https://y22s1-assignment1b-group10-ag.herokuapp.com/api/article/SearchArticle', postdate)
-        .then(res => {
-          this.setState({
-            article: res.data
-          })
-        })
-        .catch(err =>{
-          console.log('Error');
-        })
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      article: [],
     };
+  }
+  Search(Searchinput) {
+    let postdate = {
+      title: Searchinput,
+    };
+    axios
+      .post("api/article/SearchArticle", postdate)
+      .then((res) => {
+        this.setState({
+          article: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log("Error");
+      });
+  }
 
-    render(){
-        const article = this.state.article;
-        console.log("Print Book: " + article);
-        let articleList;
+  render() {
+    const article = this.state.article;
+    console.log("Print Book: " + article);
+    let articleList;
 
-        if(!article){
-            articleList = "There is no articles record!";
-        }
-        else{
-            // articles.map();
-        }
-        return (
-            <div>
-            <input ref={(input) => {this.search = input}}/>
-            <br></br>
-
-            <button onClick={()=>this.Search(this.search.value)}>Search</button>
-            {articleList}
-            </div>
-          
-        );
+    if (!article) {
+      articleList = "There is no articles record!";
+    } else {
+      // articles.map();
     }
-}export default Search;
+    return (
+      <div>
+        <input
+          ref={(input) => {
+            this.search = input;
+          }}
+        />
+        <br></br>
+
+        <button onClick={() => this.Search(this.search.value)}>Search</button>
+        {articleList}
+      </div>
+    );
+  }
+}
+export default Search;
--- frontend/src/pages/SearchArticle.js
+++ frontend/src/pages/SearchArticle.js
@@ -1,11 +1,12 @@
 import React from "react";
-import SubmissionForm from "../components/submissionForm";
+import  Search  from "../components/Search";
 
 const SearchArticle = () => {
   return (
     <div>
       <h2>Search Article</h2>
       <p>Please search </p>
+      <Search/>
     </div>
   );
 };
